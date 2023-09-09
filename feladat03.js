8021e0a043cde0ffd000d5785084dae60dfb48b7
finyo3549 <75877945+finyo3549@users.noreply.github.com>
2023-09-09 08:53:18 +0200

    Initial commit

---
+++ feladat03.js
@@ -0,0 +1,4 @@
+var lista = [0,7,16,30,41];
+console.log("A tömb elemei visszafelé:");
+lista.findLast(elem => console.log(elem));
+
---
+++ main.js
@@ -0,0 +1,6 @@
+c = 11;
+console.log(c);
+
+function osszead(szam1,szam2) {
+    return szam1 + szam2;
+}
