3f12be09df6653e63f41ed572d9fa82a21108c11
Tari Attila <finyo3549@gmail.com>
2023-09-15 11:23:08 +0200

    var helyett let-tel definiálva

--- feladat03.js
+++ feladat03.js
@@ -1,4 +1,4 @@
-var lista = [0,7,16,30,41];
+let vissza = [0,7,16,30,41];
 console.log("A tömb elemei visszafelé:");
-lista.findLast(elem => console.log(elem));
+vissza.findLast(elem => console.log(elem));
 
